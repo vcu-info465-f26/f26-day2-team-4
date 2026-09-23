700cb3ed4e20662af1e81b7a3bb29d6003d61aac
CKVCU <cknight5@vcu.edu>
2026-09-23 23:42:08 +0000

    Updated album code by changing parameters so current data is pulled from API, also changed Readme to reflect what data is pulled

--- src/fetch.py
+++ src/fetch.py
@@ -15,18 +15,8 @@ def get_most_listened_albums_today():
 
     current_date = datetime.datetime.now()
 
-    start_of_period = current_date.replace(hour=0, minute=0, second=0, microsecond=0)
-    end_of_period = current_date.replace(hour=23, minute=59, second=59, microsecond=999999)
-
-    from_timestamp = int(time.mktime(start_of_period.timetuple()))
-    to_timestamp = int(time.mktime(end_of_period.timetuple()))
-
     url = "https://api.listenbrainz.org/1/stats/sitewide/releases"
-    params = {
-        "from": from_timestamp,
-        "to": to_timestamp,
-        "count": 30
-    }
+    params = {"range":"week", "count": 30}
 
     response = requests.get(url, params=params, timeout=10)
 
